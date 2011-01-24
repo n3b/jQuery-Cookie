c55cfb8c69c0c139f8188454a15dcab304659c7f
Sebastian Tschan <sebastian.tschan@gmail.com>
2011-01-24 14:23:16 +0900

    Minor JSLint compliance update.

--- jquery.cookie.js
+++ jquery.cookie.js
@@ -20,31 +20,31 @@
 (function ($) {
     
     var getCookie = function (key, options) {
-        options = options || {};
-        var result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie),
-            decode = options.raw ? String : decodeURIComponent;
-        return result ? decode(result[1]) : null;
-    },
+            options = options || {};
+            var result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie),
+                decode = options.raw ? String : decodeURIComponent;
+            return result ? decode(result[1]) : null;
+        },
 
-    setCookie = function (key, value, options) {
-        options = options || {};
-        if (value === null) {
-            options.expires = -1;
-        }
-        if (typeof options.expires === 'number') {
-            var days = options.expires;
-            options.expires = new Date();
-            options.expires.setDate(options.expires.getDate() + days);
-        }
-        return (document.cookie = [
-            encodeURIComponent(key), '=',
-            options.raw ? String(value) : encodeURIComponent(String(value)),
-            options.expires ? '; expires=' + options.expires.toUTCString() : '',
-            options.path ? '; path=' + options.path : '',
-            options.domain ? '; domain=' + options.domain : '',
-            options.secure ? '; secure' : ''
-        ].join(''));
-    };
+        setCookie = function (key, value, options) {
+            options = options || {};
+            if (value === null) {
+                options.expires = -1;
+            }
+            if (typeof options.expires === 'number') {
+                var days = options.expires;
+                options.expires = new Date();
+                options.expires.setDate(options.expires.getDate() + days);
+            }
+            return (document.cookie = [
+                encodeURIComponent(key), '=',
+                options.raw ? String(value) : encodeURIComponent(String(value)),
+                options.expires ? '; expires=' + options.expires.toUTCString() : '',
+                options.path ? '; path=' + options.path : '',
+                options.domain ? '; domain=' + options.domain : '',
+                options.secure ? '; secure' : ''
+            ].join(''));
+        };
     
     $.cookie = function (key, value, options) {
         if (arguments.length > 1 && (value === null || typeof value !== 'object')) {
